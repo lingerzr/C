904c7c79a9e4adadec07ff8936e455d68bc42af4
Linger <ruizhu8636@gmail.com>
2025-02-05 15:56:17 +0800

    20250205-02

--- hello_world.c
+++ hello_world.c
@@ -6,6 +6,13 @@ int main()  //主函数-程序的入口-main函数有且仅有一个
     double f = 5.0;
     printf("hello b1t\n");  //打印函数-库函数用#include<stdio.h>的文件
     printf("%lf\n", f);
+    printf("%d\n", sizeof(char));
+    printf("%d\n", sizeof(short));
+    printf("%d\n", sizeof(int));
+    printf("%d\n", sizeof(long));
+    printf("%d\n", sizeof(long long));
+    printf("%d\n", sizeof(float));
+    printf("%d\n", sizeof(double));
     return 0;  //返回 0
 }
 
